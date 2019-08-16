2ffe1262154e40582b4bfc55d9b933976b248c03
Wu Nan <geek5nan@gmail.com>
2019-08-16 17:25:38 +0800

    为 androidTest 目录下的 LargeTest 添加中文注释

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/AppNavigationTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/AppNavigationTest.kt
@@ -19,6 +19,7 @@ import android.view.Gravity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
@@ -63,11 +64,13 @@ class AppNavigationTest {
 
     @Before
     fun init() {
+        //初始化虚假数据仓库
         tasksRepository = ServiceLocator.provideTasksRepository(getApplicationContext())
     }
 
     @After
     fun reset() {
+        // 重置数据仓库
         ServiceLocator.resetRepository()
     }
 
@@ -77,6 +80,7 @@ class AppNavigationTest {
      */
     @Before
     fun registerIdlingResource() {
+        //注册 Espresso 资源
         IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
         IdlingRegistry.getInstance().register(dataBindingIdlingResource)
     }
@@ -86,6 +90,7 @@ class AppNavigationTest {
      */
     @After
     fun unregisterIdlingResource() {
+        //注销 Espresso 资源
         IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
         IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
     }
@@ -93,9 +98,10 @@ class AppNavigationTest {
     @Test
     fun drawerNavigationFromTasksToStatistics() {
         // start up Tasks screen
+        // 开启任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
-
+        // 断言抽屉布局处于关闭状态后打开抽屉栏
         onView(withId(R.id.drawer_layout))
             .check(matches(isClosed(Gravity.START))) // Left Drawer should be closed.
             .perform(open()) // Open Drawer
@@ -105,17 +111,21 @@ class AppNavigationTest {
             .perform(navigateTo(R.id.statistics_fragment_dest))
 
         // Check that statistics screen was opened.
+        // 断言统计 Fragment 已正常显示
         onView(withId(R.id.statistics_layout)).check(matches(isDisplayed()))
 
+        // 断言抽屉栏关闭后打开抽屉栏
         onView(withId(R.id.drawer_layout))
             .check(matches(isClosed(Gravity.START))) // Left Drawer should be closed.
             .perform(open()) // Open Drawer
 
         // Start tasks screen.
+        // 开启任务列表 Fragment
         onView(withId(R.id.nav_view))
             .perform(navigateTo(R.id.tasks_fragment_dest))
 
         // Check that tasks screen was opened.
+        // 断言任务列表 layout 已正常显示
         onView(withId(R.id.tasks_container_layout)).check(matches(isDisplayed()))
     }
 
@@ -126,18 +136,23 @@ class AppNavigationTest {
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Check that left drawer is closed at startup
+        // 断言抽屉栏处于关闭状态
         onView(withId(R.id.drawer_layout))
             .check(matches(isClosed(Gravity.START))) // Left Drawer should be closed.
 
         // Open Drawer
+        // 获取导航按钮并点击
         onView(
+            // 通过内容描述字符串获取 Matcher 对象
             withContentDescription(
+                // 获取 Toolbar 导航内容描述字符串
                 activityScenario
                     .getToolbarNavigationContentDescription()
             )
         ).perform(click())
 
         // Check if drawer is open
+        // 断言抽屉栏已打开
         onView(withId(R.id.drawer_layout))
             .check(matches(isOpen(Gravity.START))) // Left drawer is open open.
     }
@@ -145,54 +160,67 @@ class AppNavigationTest {
     @Test
     fun statsScreen_clickOnAndroidHomeIcon_OpensNavigation() {
         // start up Tasks screen
+        // 开启任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // When the user navigates to the stats screen
+        // 跳转至统计 Fragment
         activityScenario.onActivity {
             it.findNavController(R.id.nav_host_fragment).navigate(R.id.statistics_fragment_dest)
         }
 
         // Then check that left drawer is closed at startup
+        // 断言抽屉栏处于关闭状态
         onView(withId(R.id.drawer_layout))
             .check(matches(isClosed(Gravity.START))) // Left Drawer should be closed.
 
         // When the drawer is opened
+        // 通过 toolbar 上的导航按钮打开抽屉栏
         onView(
             withContentDescription(
-                activityScenario
-                    .getToolbarNavigationContentDescription()
+//                activityScenrio
+////                    .getToolbarNavigationContentDescription()a
+                    R.string.nav_app_bar_open_drawer_description
             )
         ).perform(click())
 
         // Then check that the drawer is open
+        // 断言抽屉栏已打开
         onView(withId(R.id.drawer_layout))
             .check(matches(isOpen(Gravity.START))) // Left drawer is open open.
     }
 
     @Test
     fun taskDetailScreen_doubleUIBackButton() {
+        // 向数据仓库新增 Task 对象
         val task = Task("UI <- button", "Description")
         tasksRepository.saveTaskBlocking(task)
 
         // start up Tasks screen
+        // 开启并监控任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Click on the task on the list
+        // 通过字符串找到 Task 对象并点击
         onView(withText("UI <- button")).perform(click())
         // Click on the edit task button
+        // 通过 id 找到 fab 按钮并点击
         onView(withId(R.id.edit_task_fab)).perform(click())
 
         // Confirm that if we click "<-" once, we end up back at the task details page
+        // 点击 toolbar 返回按钮
         onView(
             withContentDescription(
                 activityScenario
                     .getToolbarNavigationContentDescription()
             )
         ).perform(click())
+        // 断言当前处于任务详情页面
         onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
 
+        // 点击返回按钮
         // Confirm that if we click "<-" a second time, we end up back at the home screen
         onView(
             withContentDescription(
@@ -200,28 +228,36 @@ class AppNavigationTest {
                     .getToolbarNavigationContentDescription()
             )
         ).perform(click())
+        // 断言当前处于任务列表页面
         onView(withId(R.id.tasks_container_layout)).check(matches(isDisplayed()))
     }
 
     @Test
     fun taskDetailScreen_doubleBackButton() {
+        // 向数据仓库新增 Task 对象
         val task = Task("Back button", "Description")
         tasksRepository.saveTaskBlocking(task)
 
         // start up Tasks screen
+        // 开启 任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Click on the task on the list
+        // 通过文字找到 View 并点击
         onView(withText("Back button")).perform(click())
         // Click on the edit task button
+        // 点击 fab 按钮
         onView(withId(R.id.edit_task_fab)).perform(click())
 
         // Confirm that if we click back once, we end up back at the task details page
+        // 触发 back 按钮
         pressBack()
+        // 断言当前处于任务详情页面
         onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
 
         // Confirm that if we click back a second time, we end up back at the home screen
+        // 触发 back 事件，并断言当前处于任务列表页面
         pressBack()
         onView(withId(R.id.tasks_container_layout)).check(matches(isDisplayed()))
     }
--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivityTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivityTest.kt
@@ -19,6 +19,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.replaceText
@@ -93,25 +94,34 @@ class TasksActivityTest {
     }
 
     @Test
+    // 测试编辑任务功能
     fun editTask() {
+        // 向数据仓库新增 Task 对象
         repository.saveTaskBlocking(Task("TITLE1", "DESCRIPTION"))
 
         // start up Tasks screen
+        // 开启 任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Click on the task on the list and verify that all the data is correct
+        // 点击标题为 TITLE1 的 TASK
         onView(withText("TITLE1")).perform(click())
+        // 断言 TASK 的内容已在任务详情页正常显示
         onView(withId(R.id.task_detail_title_text)).check(matches(withText("TITLE1")))
         onView(withId(R.id.task_detail_description_text)).check(matches(withText("DESCRIPTION")))
         onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
 
         // Click on the edit button, edit, and save
+        // 点击 fab 按钮
         onView(withId(R.id.edit_task_fab)).perform(click())
+        // 编辑 TASK 标题
         onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("NEW TITLE"))
+        // 标记 TASK 描述
         onView(withId(R.id.add_task_description_edit_text)).perform(replaceText("NEW DESCRIPTION"))
+        // 点击 fab 按钮 保存修改
         onView(withId(R.id.save_task_fab)).perform(click())
-
+        // 断言 修改后的 TASK 对象内容正常显示
         // Verify task is displayed on screen in the task list.
         onView(withText("NEW TITLE")).check(matches(isDisplayed()))
         // Verify previous task is not displayed
@@ -119,6 +129,7 @@ class TasksActivityTest {
     }
 
     @Test
+    // 测试删除任务功能
     fun createOneTask_deleteTask() {
 
         // start up Tasks screen
@@ -126,18 +137,21 @@ class TasksActivityTest {
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Add active task
+        // 添加任务
         onView(withId(R.id.add_task_fab)).perform(click())
         onView(withId(R.id.add_task_title_edit_text))
             .perform(typeText("TITLE1"), closeSoftKeyboard())
         onView(withId(R.id.add_task_description_edit_text)).perform(typeText("DESCRIPTION"))
         onView(withId(R.id.save_task_fab)).perform(click())
 
+        // 删除任务
         // Open it in details view
         onView(withText("TITLE1")).perform(click())
         // Click delete task in menu
         onView(withId(R.id.menu_delete)).perform(click())
 
         // Verify it was deleted
+        // 断言任务已删除（在任务列表中不存在 TITLE1 对应的任务）
         onView(withId(R.id.menu_filter)).perform(click())
         onView(withText(string.nav_all)).perform(click())
         onView(withText("TITLE1")).check(doesNotExist())
@@ -145,19 +159,24 @@ class TasksActivityTest {
 
     @Test
     fun createTwoTasks_deleteOneTask() {
+        // 添加任务
         repository.saveTaskBlocking(Task("TITLE1", "DESCRIPTION"))
         repository.saveTaskBlocking(Task("TITLE2", "DESCRIPTION"))
 
         // start up Tasks screen
+        // 开启 任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Open the second task in details view
+        // 打开 TITLE2 详情
         onView(withText("TITLE2")).perform(click())
         // Click delete task in menu
+        // 删除 TITLE2
         onView(withId(R.id.menu_delete)).perform(click())
 
         // Verify only one task was deleted
+        // 断言 TITLE2 删除成功（任务列表只能显示 TITLE1 不显示 TITLE2）
         onView(withId(R.id.menu_filter)).perform(click())
         onView(withText(string.nav_all)).perform(click())
         onView(withText("TITLE1")).check(matches(isDisplayed()))
@@ -167,20 +186,24 @@ class TasksActivityTest {
     @Test
     fun markTaskAsCompleteOnDetailScreen_taskIsCompleteInList() {
         // Add 1 active task
+        // 添加任务
         val taskTitle = "COMPLETED"
         repository.saveTaskBlocking(Task(taskTitle, "DESCRIPTION"))
 
         // start up Tasks screen
+        // 开启 任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Click on the task on the list
+        // 点击 COMPLETED 对应的任务，进入任务详情
         onView(withText(taskTitle)).perform(click())
-
         // Click on the checkbox in task details screen
+        // 点击完成按钮
         onView(withId(R.id.task_detail_complete_checkbox)).perform(click())
 
         // Click on the navigation up button to go back to the list
+        // 点击导航按钮，回到任务列表页面
         onView(
             withContentDescription(
                 activityScenario.getToolbarNavigationContentDescription()
@@ -188,6 +211,7 @@ class TasksActivityTest {
         ).perform(click())
 
         // Check that the task is marked as completed
+        // 断言 COMPLETE 标题对应的 TASK 中的 checkbox 已勾选
         onView(allOf(withId(R.id.complete_checkbox), hasSibling(withText(taskTitle))))
             .check(matches(isChecked()))
     }
@@ -195,19 +219,24 @@ class TasksActivityTest {
     @Test
     fun markTaskAsActiveOnDetailScreen_taskIsActiveInList() {
         // Add 1 completed task
+        // 添加标题为 ACTIVE 的任务
         val taskTitle = "ACTIVE"
         repository.saveTaskBlocking(Task(taskTitle, "DESCRIPTION", true))
 
         // start up Tasks screen
+        // 开启任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Click on the task on the list
+        // 进入标题为 ACTIVE 任务的详情
         onView(withText(taskTitle)).perform(click())
         // Click on the checkbox in task details screen
+        // 勾选完成按钮
         onView(withId(R.id.task_detail_complete_checkbox)).perform(click())
 
         // Click on the navigation up button to go back to the list
+        // 返回任务列表页面
         onView(
             withContentDescription(
                 activityScenario.getToolbarNavigationContentDescription()
@@ -215,6 +244,7 @@ class TasksActivityTest {
         ).perform(click())
 
         // Check that the task is marked as active
+        // 断言 ACTIVE 对应任务的 checkbox 未勾选
         onView(allOf(withId(R.id.complete_checkbox), hasSibling(withText(taskTitle))))
             .check(matches(not(isChecked())))
     }
@@ -222,18 +252,22 @@ class TasksActivityTest {
     @Test
     fun markTaskAsCompleteAndActiveOnDetailScreen_taskIsActiveInList() {
         // Add 1 active task
+        // 添加任务
         val taskTitle = "ACT-COMP"
         repository.saveTaskBlocking(Task(taskTitle, "DESCRIPTION"))
 
         // start up Tasks screen
+        // 开启任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
-
         // Click on the task on the list
+        // 进入 ACT-COMP 任务详情
         onView(withText(taskTitle)).perform(click())
         // Click on the checkbox in task details screen
+        // 勾选
         onView(withId(R.id.task_detail_complete_checkbox)).perform(click())
         // Click again to restore it to original state
+        // 取消勾选
         onView(withId(R.id.task_detail_complete_checkbox)).perform(click())
 
         // Click on the navigation up button to go back to the list
@@ -244,6 +278,7 @@ class TasksActivityTest {
         ).perform(click())
 
         // Check that the task is marked as active
+        // 断言 未勾选
         onView(allOf(withId(R.id.complete_checkbox), hasSibling(withText(taskTitle))))
             .check(matches(not(isChecked())))
     }
@@ -251,28 +286,34 @@ class TasksActivityTest {
     @Test
     fun markTaskAsActiveAndCompleteOnDetailScreen_taskIsCompleteInList() {
         // Add 1 completed task
+        // 添加任务, 初始状态为 已完成
         val taskTitle = "COMP-ACT"
         repository.saveTaskBlocking(Task(taskTitle, "DESCRIPTION", true))
 
         // start up Tasks screen
+        // 开启任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Click on the task on the list
+        // 进入任务详情
         onView(withText(taskTitle)).perform(click())
         // Click on the checkbox in task details screen
+        // 勾选  ，状态变为 未勾选
         onView(withId(R.id.task_detail_complete_checkbox)).perform(click())
         // Click again to restore it to original state
+        // 反勾选，状态变为 已勾选
         onView(withId(R.id.task_detail_complete_checkbox)).perform(click())
 
         // Click on the navigation up button to go back to the list
+        // 返回任务列表
         onView(
             withContentDescription(
                 activityScenario.getToolbarNavigationContentDescription()
             )
         ).perform(click())
-
         // Check that the task is marked as active
+        // 断言 COMP-ACT 对应任务的状态为已勾选
         onView(allOf(withId(R.id.complete_checkbox), hasSibling(withText(taskTitle))))
             .check(matches(isChecked()))
     }
@@ -280,16 +321,19 @@ class TasksActivityTest {
     @Test
     fun createTask() {
         // start up Tasks screen
+        // 开启任务列表 Activity
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         // Click on the "+" button, add details, and save
+        // 点击 fab
         onView(withId(R.id.add_task_fab)).perform(click())
+        // 新建 TASK
         onView(withId(R.id.add_task_title_edit_text))
             .perform(typeText("title"), closeSoftKeyboard())
         onView(withId(R.id.add_task_description_edit_text)).perform(typeText("description"))
         onView(withId(R.id.save_task_fab)).perform(click())
-
+        // 断言新建的 TASK 已显示
         // Then verify task is displayed on screen
         onView(withText("title")).check(matches(isDisplayed()))
     }
